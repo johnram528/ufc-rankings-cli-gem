ae6690b4747b96e9e58bf32ec2c8934c22bd0d8e
johnram528 <johnram528@gmail.com>
2016-07-06 23:57:27 -0400

    ufc refactor

--- lib/ufc_rankings/cli.rb
+++ lib/ufc_rankings/cli.rb
@@ -1,4 +1,5 @@
 class UfcRankings::CLI
+  @@division_titles = ["Pound for Pound", "Flyweight", "Bantamweight", "Featherweight", "Lightweight", "Welterweight", "Middleweight", "Light Heavyweight", "Heavyweight", "Women's Strawweight", "Women's Bantamweight"]
   def call
     UfcRankings::Rankings.scrape_rankings
     welcome
@@ -11,7 +12,7 @@ class UfcRankings::CLI
   end
 
   def menu
-    puts "Please select the weight class rankings you'd like to see from the list below by entering the number assigned to each class. ."
+    puts "Please select the weight class rankings you'd like to see from the list below by entering the number assigned to each class."
     puts ""
     list_classes
     input = gets.strip
@@ -28,56 +29,19 @@ class UfcRankings::CLI
   end
 
   def list_classes 
-    puts "1.Pound for Pound "
-    puts "2.Flyweight "
-    puts "3.Bantamweight"
-    puts "4.Featherweight "
-    puts "5.Lightweight "
-    puts "6.Welterweight "
-    puts "7.Middleweight "
-    puts "8.Light Heavyweight "
-    puts "9.Heavyweight "
-    puts "10.Women's Strawweight "
-    puts "11.Women's Bantamweight"
-    puts ""
+    0.upto(10) {|i| puts "#{i+1}.#{@@division_titles[i]}"}
   end
-    
+   
   def show_rankings(num)
-    case num
-    when 1
-      puts "Pound for Pound Rankings"
-      UfcRankings::Rankings.p4p
-    when 2
-      puts "Flyweight Rankings"
-      UfcRankings::Rankings.fly
-    when 3
-      puts "Bantameweight Rankings"
-      UfcRankings::Rankings.bantam
-    when 4
-      puts "Featherweight Rankings"
-      UfcRankings::Rankings.feather
-    when 5
-      puts "Lightweight Rankings"
-      UfcRankings::Rankings.light
-    when 6
-      puts "Welterweight Rankings"
-      UfcRankings::Rankings.welter
-    when 7
-      puts "Middleweight Rankings"
-      UfcRankings::Rankings.middle
-    when 8
-      puts "Light Heavyweight Rankings"
-      UfcRankings::Rankings.light_heavy
-    when 9 
-      puts "Heavyweight Rankings"
-      UfcRankings::Rankings.heavy
-    when 10
-      puts "Women's Strawweight"
-      UfcRankings::Rankings.womens_straw
-    when 11
-      puts "Women's Bantamweight"
-      UfcRankings::Rankings.womens_bantam
+    index = num - 1
+    puts @@division_titles[index]
+    UfcRankings::Rankings.divisions[index].each_with_index do |fighter, i|
+      if i == 0
+    puts "Champion: #{fighter}"  
+      else 
+    puts "#{i+1}. #{fighter}"
     end
+  end
   end
 
   def exit
--- lib/ufc_rankings/rankings.rb
+++ lib/ufc_rankings/rankings.rb
@@ -1,99 +1,28 @@
 class UfcRankings::Rankings
 
-  @@champions = []
-  @@p4p = []
-  @@fly = []
-  @@bantam = []
-  @@feather = []
-  @@light = []
-  @@welter = []
-  @@middle = []
-  @@light_heavy = []
-  @@heavy = []
-  @@womens_straw = []
-  @@womens_bantam = []
+  @@divisions = {}
 
+  0.upto(10) {|i| @@divisions[i] = Array.new}
 
   def self.scrape_rankings
     doc = Nokogiri::HTML(open("http://www.ufc.com/rankings"))
-    rankings = doc.css(".ranking-list")
-    pound_for_pound = rankings[0].css("a").children.each {|fighter| @@p4p << fighter.text.strip.split.join(" ")}
-    rankings[1].css("a").children.each {|fighter| @@fly << fighter.text.strip.split.join(" ")}
-    rankings[2].css("a").children.each {|fighter| @@bantam << fighter.text.strip.split.join(" ")}
-    rankings[3].css("a").children.each {|fighter| @@feather << fighter.text.strip.split.join(" ")}
-    rankings[4].css("a").children.each {|fighter| @@light << fighter.text.strip.split.join(" ")}
-    rankings[5].css("a").children.each {|fighter| @@welter << fighter.text.strip.split.join(" ")}
-    rankings[6].css("a").children.each {|fighter| @@middle << fighter.text.strip.split.join(" ")}
-    rankings[7].css("a").children.each {|fighter| @@light_heavy << fighter.text.strip.split.join(" ")}
-    rankings[8].css("a").children.each {|fighter| @@heavy << fighter.text.strip.split.join(" ")}
-    rankings[9].css("a").children.each {|fighter| @@womens_straw << fighter.text.strip.split.join(" ")}
-    rankings[10].css("a").children.each {|fighter| @@womens_bantam << fighter.text.strip.split.join(" ")}
-    @@champions << @@fly[0] << @@bantam[0] << @@feather[0] << @@light[0] << @@welter[0] << @@middle[0] << @@light_heavy[0] << @@heavy[0] << @@womens_straw[0] << @@womens_bantam[0] 
-
-
+    rankings = doc.css(".ranking-list") 
+    0.upto(10) {|i| rankings[i].css("a").children.each {|fighter| @@divisions[i] << fighter.text.strip.split.join(" ")}}
   end
 
   def self.p4p
     @@p4p.each_with_index {|fighter, i|puts "#{i+1}. #{fighter}"}
   end
 
-  def self.fly 
-   puts "Champion: #{@@champions[0]}"
-   @@fly.shift
-   @@fly.each_with_index {|fighter, i|puts "#{i+1}. #{fighter}"}
+
+  def self.divisions
+    @@divisions
+    # @@divisions[index].each_with_index do |fighter, i|
+    #   if i == 0
+    # puts "Champion: #{fighter}"  
+    #   else 
+    # puts "#{i+1}. #{fighter}"
+    # end
   end
 
-  def self.bantam
-   puts "Champion: #{@@champions[1]}"
-   @@bantam.shift
-   @@bantam.each_with_index {|fighter, i|puts "#{i+1}. #{fighter}"}
-  end
-
-  def self.feather
-   puts "Champion: #{@@champions[2]}"
-   @@feather.shift
-   @@feather.each_with_index {|fighter, i|puts "#{i+1}. #{fighter}"}
- end
-
- def self.light
-   puts "Champion: #{@@champions[3]}"
-   @@light.shift
-   @@light.each_with_index {|fighter, i|puts "#{i+1}. #{fighter}"}
- end
-
- def self.welter
-   puts "Champion: #{@@champions[4]}"
-   @@welter.shift
-   @@welter.each_with_index {|fighter, i|puts "#{i+1}. #{fighter}"}
- end 
-
- def self.middle
-   puts "Champion: #{@@champions[5]}"
-   @@middle.shift
-   @@middle.each_with_index {|fighter, i|puts "#{i+1}. #{fighter}"}
- end
-
- def self.light_heavy
-   puts "Champion: #{@@champions[6]}"
-   @@light_heavy.shift
-   @@light_heavy.each_with_index {|fighter, i|puts "#{i+1}. #{fighter}"}
- end
-
- def self.heavy
-   puts "Champion: #{@@champions[7]}"
-   @@heavy.shift
-   @@heavy.each_with_index {|fighter, i|puts "#{i+1}. #{fighter}"}
- end
-
- def self.womens_straw
-   puts "Champion: #{@@champions[8]}"
-   @@womens_straw.shift
-   @@womens_straw.each_with_index {|fighter, i|puts "#{i+1}. #{fighter}"}
- end
-
- def self.womens_bantam
-   puts "Champion: #{@@champions[9]}"
-   @@womens_bantam.shift
-   @@womens_bantam.each_with_index {|fighter, i|puts "#{i+1}. #{fighter}"}
- end
 end
